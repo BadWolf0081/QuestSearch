59edad992a302853fea00ad7aef52143636be755
Robbie <57416363+BadWolf0081@users.noreply.github.com>
2024-07-24 15:47:43 -0300

    added lures
    added support for searching for lures! with type and time left.

--- qs.py
+++ qs.py
@@ -71,6 +71,24 @@ async def get_data(area, mon_id):
         quests = await cur.fetchall()
     await conn.ensure_closed()
     return quests
+    
+async def get_lures(area):
+    conn = await aiomysql.connect(host=config['db_host'],user=config['db_user'],password=config['db_pass'],db=config['db_dbname'],port=config['db_port'])
+    cur = await conn.cursor()
+    async with conn.cursor() as cur:
+        await cur.execute(f"SELECT lure_expire_timestamp, lure_id, lat, lon, name FROM pokestop WHERE ST_Contains(ST_GeomFromText('POLYGON(({area[0]}))'), POINT(lat,lon)) AND lure_expire_timestamp >= UNIX_TIMESTAMP() ORDER BY name;")
+        quests = await cur.fetchall()
+    await conn.ensure_closed()
+    return quests
+    
+async def get_datarocketquery(area, mon_id):
+    conn = await aiomysql.connect(host=config['db_host'],user=config['db_user'],password=config['db_pass'],db=config['db_dbname'],port=config['db_port'])
+    cur = await conn.cursor()
+    async with conn.cursor() as cur:
+        await cur.execute(f"SELECT quest_rewards, quest_template, lat, lon, name, id FROM pokestop WHERE quest_pokemon_id = {mon_id} AND ST_Contains(ST_GeomFromText('POLYGON(({area[0]}))'), POINT(lat,lon)) AND updated >= UNIX_TIMESTAMP()-86400 ORDER BY quest_pokemon_id ASC, name;")
+        quests = await cur.fetchall()
+    await conn.ensure_closed()
+    return rocket_return
 
 async def get_alt_data(area, mon_id):
     conn = await aiomysql.connect(host=config['db_host'],user=config['db_user'],password=config['db_pass'],db=config['db_dbname'],port=config['db_port'])
@@ -217,6 +235,10 @@ async def quest(ctx, areaname = "", *, reward):
         embed = discord.Embed(title=bot.locale['mega'], description=text)
         embed.set_image(url="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c3c4d331234507.564a1d23db8f9.gif")
         embed.set_footer(text=loading, icon_url="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c3c4d331234507.564a1d23db8f9.gif")
+    elif reward.startswith("Lure") or reward.startswith("lure"):
+        embed = discord.Embed(title=bot.locale['active_lures'], description=text)
+        embed.set_image(url="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c3c4d331234507.564a1d23db8f9.gif")
+        embed.set_footer(text=loading, icon_url="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c3c4d331234507.564a1d23db8f9.gif")
     elif reward.startswith("Showcase") or reward.startswith("showcase"):
         embed = discord.Embed(title=bot.locale['showcase'], description=text)
         embed.set_image(url="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c3c4d331234507.564a1d23db8f9.gif")
@@ -290,6 +312,10 @@ async def quest(ctx, areaname = "", *, reward):
             embed.title = f"{mon.name} {bot.locale['showcase']} - {area[1]}"
             embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}misc/showcase.png")
             quests = await get_datashow(area)
+        elif reward.startswith("Lure") or reward.startswith("lure"):
+            embed.title = f"{bot.locale['active_lures']} - {area[1]}"
+            embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}pokestop/501.png")
+            quests = await get_lures(area)
         elif reward.startswith("Route") or reward.startswith("route"):
             embed.title = f"{bot.locale['routes']} - {area[1]}"
             embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}misc/route-start.png")
@@ -462,6 +488,65 @@ async def quest(ctx, areaname = "", *, reward):
                     embed.title = f"{mon.name} Quests SHINY DETECTED!! - {area[1]}"
                 else:
                     entry = f"[{stop_name}]({map_url})\n"
+                if length + len(entry) >= 2400:
+                    if shiny:
+                        text = entry + text
+                        length = length + len(entry)
+                    else:
+                        theend = f" lots more ..."
+                        text = text + theend
+                        break
+                else:
+                    if shiny:
+                        text = entry + text
+                        length = length + len(entry)
+                    else:
+                        text = text + entry
+                        length = length + len(entry)
+    elif reward.startswith("Lure") or reward.startswith("lure"):
+        for lure_expire_timestamp, lure_id, lat, lon, stop_name in quests:
+            tstamp1 = datetime.fromtimestamp(lure_expire_timestamp)
+            tstamp2 = datetime.now()
+            td = tstamp1 - tstamp2
+            left = int(round(td.total_seconds() / 60))
+            found_rewards = True
+            shiny = False
+            mon_id = 0
+            item_id = 0
+            reward_items = 99996
+            emote_name = f"e{mon_id}"
+            emote_img = f"{bot.config['mon_icon_repo']}pokestop/501.png"
+            if lure_id == 501:
+                luretype= "Normal"
+            elif lure_id == 502:
+                luretype= "Ice"
+            elif lure_id == 503:
+                luretype= "Grass"
+            elif lure_id == 504:
+                luretype= "Electric"
+            elif lure_id == 505:
+                luretype= "Water"
+            elif lure_id == 506:
+                luretype= "Gold"
+            if found_rewards:
+                if len(stop_name) >= 31:
+                    stop_name = stop_name[0:30]
+                lat_list.append(lat)
+                lon_list.append(lon)
+
+                if bot.config['use_map']:
+                    map_url = bot.map_url.quest(lat, lon, stop_id)
+                else:
+                    map_url = f"https://www.google.com/maps/search/?api=1&query={lat},{lon}"
+
+                if item_id in items:
+                    entry = f"[{stop_name} **{luretype}**]({map_url})\n"
+                elif shiny:
+                    entry = f"[{stop_name} **SHINY**]({map_url})\n"
+                    embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}pokemon/{str(mon.id)}_s.png")
+                    embed.title = f"{mon.name} Quests SHINY DETECTED!! - {area[1]}"
+                else:
+                    entry = f"[{stop_name} - {luretype} - {left} Min]({map_url})\n"
                 if length + len(entry) >= 2400:
                     if shiny:
                         text = entry + text
@@ -650,6 +735,143 @@ async def quest(ctx, areaname = "", *, reward):
                     else:
                         text = text + entry
                         length = length + len(entry)
+    embed.description = text
+    image = "https://raw.githubusercontent.com/ccev/dp_emotes/master/blank.png"
+    if length > 0:
+        if bot.config['use_static']:
+            if bot.config['static_provider'] == "mapbox":
+                guild = await bot.fetch_guild(bot.config['host_server'])
+                existing_emotes = await guild.fetch_emojis()
+                emote_exist = False
+                for existing_emote in existing_emotes:
+                    if emote_name == existing_emote.name:
+                        emote_exist = True
+                if not emote_exist:
+                    try:
+                        image = await Admin.download_url("", emote_img)
+                        emote = await guild.create_custom_emoji(name=emote_name, image=image)
+                        emote_ref = f"<:{emote.name}:{emote.id}>"
+
+                        if emote_name in bot.custom_emotes:
+                            bot.custom_emotes[emote_name] = emote_ref
+                        else:
+                            bot.custom_emotes.update({emote_name: emote_ref})
+                    except Exception as err:
+                        print(err)
+                        print(f"Error while importing emote {emote_name}")
+
+                image = await bot.static_map.quest(lat_list, lon_list, reward_items, reward_mons, bot.custom_emotes)
+
+                if not emote_exist:
+                    await emote.delete()
+                    bot.custom_emotes.pop(emote_name)
+
+            elif bot.config['static_provider'] == "tileserver":
+                image = await bot.static_map.quest(lat_list, lon_list, reward_items, reward_mons, bot.custom_emotes)
+    else:
+        embed.description = bot.locale["no_quests_found"]
+
+    embed.set_footer(text=footer_text)
+    embed.set_image(url=image)
+
+    await message.edit(embed=embed)
+    
+@bot.command(pass_context=True, aliases=bot.config['rocket_aliases'])
+async def rocket(ctx, areaname = "", *, reward):
+    if not isUser(ctx.author.roles, ctx.channel.id):
+        print(f"@{ctx.author.name} tried to use !rocket but is no user")
+        return
+    footer_text = ""
+    text = ""
+    loading = bot.locale['loading_quests']
+
+    area = get_area(areaname)
+    if not area[1] == bot.locale['all']:
+        footer_text = area[1]
+        loading = f"{loading} • {footer_text}"
+
+    print(f"@{ctx.author.name} requested {reward} Rocket Stops for area {area[1]}")
+
+    if area[1] == "Unknown Area":
+        embed = discord.Embed(title=bot.locale['no_area_found'], description=text)
+    else:
+        embed = discord.Embed(title=bot.locale['rocket'], description=text)
+        embed.set_image(url="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c3c4d331234507.564a1d23db8f9.gif")
+        embed.set_footer(text=loading, icon_url="https://mir-s3-cdn-cf.behance.net/project_modules/disp/c3c4d331234507.564a1d23db8f9.gif")
+    message = await ctx.send(embed=embed)
+    
+    items = list()
+    mons = list()
+    item_found = False
+    mon = details(reward, bot.config['mon_icon_repo'], bot.config['language'])
+    embed.title = f"{mon.name} {bot.locale['rocket']} - {area[1]}"
+    embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}pokemon/{str(mon.id)}.png")
+    rocketq = await get_datarocketquery(area, mon.id)
+    mons.append(mon.id)
+    
+    length = 0
+    reward_mons = list()
+    reward_items = list()
+    lat_list = list()
+    lon_list = list()
+
+    embed.description = text
+
+    for rocket_json, rocket_text, lat, lon, stop_name, stop_id in rocketq:
+        rocket_json = json.loads(rocket_json)
+        found_rewards = True
+        shiny = False
+        mon_id = 0
+        item_id = 0
+        if 'pokemon_id' in rocket_json[0]["info"]:
+                mon_id = rocket_json[0]["info"]["pokemon_id"]
+        if 'shiny' in rocket_json[0]["info"]:
+                shiny = rocket_json[0]["info"]["shiny"]
+        elif mon_id in mons and shiny:
+            reward_mons.append([mon_id, lat, lon])
+            emote_name = f"m{mon_id}"
+            emote_img = f"{bot.config['mon_icon_repo']}pokemon/{str(mon.id)}_s.png"
+        elif mon_id in mons:
+            reward_mons.append([mon_id, lat, lon])
+            emote_name = f"m{mon_id}"
+            emote_img = f"{bot.config['mon_icon_repo']}pokemon/{str(mon.id)}.png"
+        else:
+            found_rewards = False
+        if found_rewards:
+            if len(stop_name) >= 31:
+                stop_name = stop_name[0:30]
+            lat_list.append(lat)
+            lon_list.append(lon)
+
+            if bot.config['use_map']:
+                map_url = bot.map_url.quest(lat, lon, stop_id)
+            else:
+                map_url = f"https://www.google.com/maps/search/?api=1&query={lat},{lon}"
+
+            if item_id in items:
+                entry = f"[{stop_name} **{amount}**]({map_url})\n"
+            elif shiny:
+                entry = f"[{stop_name} **SHINY**]({map_url})\n"
+                embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}pokemon/{str(mon.id)}_s.png")
+                embed.title = f"{mon.name} Rocket Reward SHINY DETECTED!?! - {area[1]}"
+            else:
+                entry = f"[{stop_name}]({map_url})\n"
+            if length + len(entry) >= 2400:
+                if shiny:
+                    text = entry + text
+                    length = length + len(entry)
+                else:
+                    theend = f" lots more ..."
+                    text = text + theend
+                    break
+            else:
+                if shiny:
+                    text = entry + text
+                    length = length + len(entry)
+                else:
+                    text = text + entry
+                    length = length + len(entry)
+        
     embed.description = text
     image = "https://raw.githubusercontent.com/ccev/dp_emotes/master/blank.png"
     if length > 0:
