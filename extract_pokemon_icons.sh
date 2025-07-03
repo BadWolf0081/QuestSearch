66572e35d9537bc73f7362a9a54ef02aadbbe94a
Robbie <57416363+BadWolf0081@users.noreply.github.com>
2025-07-02 23:05:07 -0300

    Update extract_pokemon_icons.sh

--- extract_pokemon_icons.sh
+++ extract_pokemon_icons.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-# filepath: c:\Users\robbi\OneDrive\Documents\GitHub\QuestSearch\extract_pokemon_icons.sh
 
 INPUT="data/api.json"
 OUTPUT="pokemon_icons.json"
 
 awk '
-    match($0, /<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<img src="(pokemon\/[^"]+)"/, arr) {
+    # For each table row, extract the relevant fields and the img src
+    match($0, /<tr>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<td>([^<]*)<\/td>.*<img src="(pokemon\/[^"]+)"/, arr) {
         name=arr[1]
         form=arr[3]
         costume=arr[4]
-        img=arr[5]
+        img=arr[9]
         # Remove leading/trailing whitespace
         gsub(/^ +| +$/, "", name)
         gsub(/^ +| +$/, "", form)
         gsub(/^ +| +$/, "", costume)
-        print "{\"name\": \"" name "\", \"form\": \"" form "\", \"costume\": \"" costume "\", \"img\": \"" img "\"}"
+        # Use img as key to ensure uniqueness
+        if (!seen[img]++) {
+            print "{\"name\": \"" name "\", \"form\": \"" form "\", \"costume\": \"" costume "\", \"img\": \"" img "\"}"
+        }
     }
 ' "$INPUT" | jq -s '.' > "$OUTPUT"
 
