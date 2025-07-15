6cf1b0a335668747c562ec8944532229301fb564
Robbie <57416363+BadWolf0081@users.noreply.github.com>
2025-07-15 07:00:47 -0300

    Update qs.py

--- qs.py
+++ qs.py
@@ -376,6 +376,40 @@ async def quest(ctx, areaname="", *, args=""):
             embed.title = f"{bot.locale['giovani']} - {area[1]}"
             embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}invasion/44.png")
             quests = await get_datagiovani(bot.config, area)
+            length = 0
+            reward_mons = []
+            lat_list = []
+            lon_list = []
+            text = ""
+            for lat, lon, stop_name, stop_id, expiration, character in quests:
+                found_rewards = True
+                mon_id = 99944
+                reward_mons.append([mon_id, lat, lon])
+                emote_name = f"r{mon_id}"
+                emote_img = f"{bot.config['mon_icon_repo']}invasion/44.png"
+                end = datetime.fromtimestamp(expiration).strftime(bot.locale['time_format_hm'])
+                text, length, stop = add_quest_entry(
+                    stop_name, lat, lon, stop_id, 0, [], None, False, length, text,
+                    max_len=26, extra=end
+                )
+                lat_list.append(lat)
+                lon_list.append(lon)
+                if stop:
+                    break
+            embed.description = text or bot.locale["no_quests_found"]
+            embed.set_footer(text=footer_text)
+            embed.set_image(url="https://raw.githubusercontent.com/ccev/dp_emotes/master/blank.png")
+            message = await ctx.send(embed=embed)
+            # Static map for Giovanni
+            if bot.config['use_static'] and reward_mons:
+                if bot.config['static_provider'] == "mapbox":
+                    image = await bot.static_map.quest(lat_list, lon_list, [], reward_mons, bot.custom_emotes)
+                elif bot.config['static_provider'] == "tileserver":
+                    image = await bot.static_map.quest(lat_list, lon_list, [], reward_mons, bot.custom_emotes)
+                embed.set_image(url=image)
+                await message.edit(embed=embed)
+            return
+
         elif reward_lower.startswith("sierra"):
             embed.title = f"{bot.locale['leaders']} - {area[1]}"
             embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}invasion/43.png")
@@ -404,7 +438,7 @@ async def quest(ctx, areaname="", *, args=""):
             embed.set_footer(text=footer_text)
             embed.set_image(url="https://raw.githubusercontent.com/ccev/dp_emotes/master/blank.png")
             message = await ctx.send(embed=embed)
-            # Static map for leaders
+            # Static map for Sierra
             if bot.config['use_static'] and reward_mons:
                 if bot.config['static_provider'] == "mapbox":
                     image = await bot.static_map.quest(lat_list, lon_list, [], reward_mons, bot.custom_emotes)
@@ -413,10 +447,16 @@ async def quest(ctx, areaname="", *, args=""):
                 embed.set_image(url=image)
                 await message.edit(embed=embed)
             return
+
         elif reward_lower.startswith("arlo"):
             embed.title = f"{bot.locale['leaders']} - {area[1]}"
             embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}invasion/42.png")
             quests = await get_dataleaders(bot.config, area, 42)
+            length = 0
+            reward_mons = []
+            lat_list = []
+            lon_list = []
+            text = ""
             for lat, lon, stop_name, stop_id, expiration, character in quests:
                 found_rewards = True
                 mon_id = 99942
@@ -425,18 +465,36 @@ async def quest(ctx, areaname="", *, args=""):
                 emote_img = f"{bot.config['mon_icon_repo']}invasion/42.png"
                 end = datetime.fromtimestamp(expiration).strftime(bot.locale['time_format_hm'])
                 text, length, stop = add_quest_entry(
-                    stop_name, lat, lon, stop_id, 0, items, None, False, length, text,
+                    stop_name, lat, lon, stop_id, 0, [], None, False, length, text,
                     max_len=26, extra=end
                 )
                 lat_list.append(lat)
                 lon_list.append(lon)
                 if stop:
                     break
+            embed.description = text or bot.locale["no_quests_found"]
+            embed.set_footer(text=footer_text)
+            embed.set_image(url="https://raw.githubusercontent.com/ccev/dp_emotes/master/blank.png")
+            message = await ctx.send(embed=embed)
+            # Static map for Arlo
+            if bot.config['use_static'] and reward_mons:
+                if bot.config['static_provider'] == "mapbox":
+                    image = await bot.static_map.quest(lat_list, lon_list, [], reward_mons, bot.custom_emotes)
+                elif bot.config['static_provider'] == "tileserver":
+                    image = await bot.static_map.quest(lat_list, lon_list, [], reward_mons, bot.custom_emotes)
+                embed.set_image(url=image)
+                await message.edit(embed=embed)
+            return
 
         elif reward_lower.startswith("cliff"):
             embed.title = f"{bot.locale['leaders']} - {area[1]}"
             embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}invasion/41.png")
             quests = await get_dataleaders(bot.config, area, 41)
+            length = 0
+            reward_mons = []
+            lat_list = []
+            lon_list = []
+            text = ""
             for lat, lon, stop_name, stop_id, expiration, character in quests:
                 found_rewards = True
                 mon_id = 99941
@@ -445,13 +503,26 @@ async def quest(ctx, areaname="", *, args=""):
                 emote_img = f"{bot.config['mon_icon_repo']}invasion/41.png"
                 end = datetime.fromtimestamp(expiration).strftime(bot.locale['time_format_hm'])
                 text, length, stop = add_quest_entry(
-                    stop_name, lat, lon, stop_id, 0, items, None, False, length, text,
+                    stop_name, lat, lon, stop_id, 0, [], None, False, length, text,
                     max_len=26, extra=end
                 )
                 lat_list.append(lat)
                 lon_list.append(lon)
                 if stop:
                     break
+            embed.description = text or bot.locale["no_quests_found"]
+            embed.set_footer(text=footer_text)
+            embed.set_image(url="https://raw.githubusercontent.com/ccev/dp_emotes/master/blank.png")
+            message = await ctx.send(embed=embed)
+            # Static map for Cliff
+            if bot.config['use_static'] and reward_mons:
+                if bot.config['static_provider'] == "mapbox":
+                    image = await bot.static_map.quest(lat_list, lon_list, [], reward_mons, bot.custom_emotes)
+                elif bot.config['static_provider'] == "tileserver":
+                    image = await bot.static_map.quest(lat_list, lon_list, [], reward_mons, bot.custom_emotes)
+                embed.set_image(url=image)
+                await message.edit(embed=embed)
+            return
         elif mon.name == "Kecleon":
             embed.title = f"{mon.name} {bot.locale['eventstop']} - {area[1]}"
             embed.set_thumbnail(url=f"{bot.config['mon_icon_repo']}pokemon/{str(mon.id)}.png")
